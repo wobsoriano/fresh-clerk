3c99e4d2037a3281fed1c62540d06489811d7380
wobsoriano <sorianorobertc@gmail.com>
2024-09-28 15:53:11 -0700

    test fix

--- islands/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ClerkProvider } from './ClerkProvider.tsx'
-export { default as SignedIn } from './SignedIn.tsx'
-export { default as SignIn } from './SignIn.tsx'
-export { default as UserButton } from './UserButton.tsx'
