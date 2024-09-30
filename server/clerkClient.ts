e1c847fa228e7f2a3620a906d77f4b805bd3775d
wobsoriano <sorianorobertc@gmail.com>
2024-09-29 21:13:30 -0700

    add clerkClient doc

--- server/clerkClient.ts
+++ server/clerkClient.ts
@@ -1,6 +1,10 @@
 import { createClerkClient } from '../deps.ts';
 import { API_URL, API_VERSION, JWT_KEY, SECRET_KEY } from './constants.ts';
 
+/**
+ * Clerk JavaScript Backend SDK client instance
+ * @see {@link https://clerk.com/docs/references/backend/overview JavaScript Backend SDK}
+ */
 export const clerkClient = createClerkClient({
   secretKey: SECRET_KEY,
   apiUrl: API_URL,
